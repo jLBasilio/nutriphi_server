8f346dc0cee0efe381016409e2c722a7581808fc
jLBasilio <jlcbasilio@gmail.com>
2019-03-30 18:03:54 +0800

    Add route for count

--- src/routes/food/food.router.ts
+++ src/routes/food/food.router.ts
@@ -1,20 +1,19 @@
 "use strict";
 import { Router } from "express";
 import { Request, Response } from "express";
-import { getManager, Like } from "typeorm";
+import { getRepository, Like } from "typeorm";
 import { Food } from "../../database/entities/Food";
 
 const router = Router();
 
 router.get("/find", async (req, res) => {
   try {
-
     let result;
     const { skip, take, foodClass } = req.query;
     if (!foodClass) {
-      result = await getManager().getRepository(Food).find();
+      result = await getRepository(Food).find({ skip, take });
     } else {
-      result = await getManager().getRepository(Food).find({
+      result = await getRepository(Food).find({
         where: [
           { primaryClassification: Like(`%${foodClass}%`) },
           { secondaryClassification: Like(`%${foodClass}%`) }
@@ -23,7 +22,6 @@ router.get("/find", async (req, res) => {
         take
       });
     }
-
     if (!result) {
       const data = {
         status: 404,
@@ -43,11 +41,54 @@ router.get("/find", async (req, res) => {
   }
 });
 
+router.get("/find/count", async (req, res) => {
+  try {
+    const { foodClass } = req.query;
+
+    let result;
+    if (!foodClass) {
+      result = await getRepository(Food)
+        .createQueryBuilder("food")
+        .select("COUNT (*)", "count")
+        .getRawOne();
+    } else {
+      result = await getRepository(Food)
+        .createQueryBuilder("food")
+        .select("COUNT (*)", "count")
+        .where(`primaryClassification LIKE '%${foodClass}%'
+          OR
+          secondaryClassification LIKE '%${foodClass}%'
+        `)
+        .getRawOne();
+    }
+
+    if (result.count) {
+      result.count = parseInt(result.count, 10);
+    }
+
+    if (!result) {
+      const data = {
+        status: 404,
+        message: "Zero count"
+      };
+      res.status(data.status).json(data);
+    } else {
+      const data = {
+        status: 200,
+        message: "Foods found",
+        data: result
+      };
+      res.status(data.status).json(data);
+    }
+  } catch (err) {
+    res.status(err.status).json(err);
+  }
+});
+
 router.post("/add", async (req, res) => {
   try {
 
-    // console.log(req.body);
-    const result = await getManager().getRepository(Food).save(req.body);
+    const result = await getRepository(Food).save(req.body);
     if (!result) {
       const data = {
         status: 500,
